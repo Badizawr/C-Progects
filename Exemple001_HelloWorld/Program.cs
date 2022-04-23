5f20991b6e8be7a9acd1b52cfa84a40725449f14
Badizawr <vicodin-13@yandex.ua>
2022-04-23 16:24:42 +0500

    createmade an explanation

--- Exemple001_HelloWorld/Program.cs
+++ Exemple001_HelloWorld/Program.cs
@@ -1,2 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет," + username);
+
+//$ git merge newcode Already up to date.
