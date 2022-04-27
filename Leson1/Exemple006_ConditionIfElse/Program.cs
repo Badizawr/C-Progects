ba9e15477bb4da522aa78b913e24ae2fe69008b2
Badizawr <vicodin-13@yandex.ua>
2022-04-27 16:56:51 +0500

    add Exemple 006 IfElse

---
+++ Leson1/Exemple006_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿//Особое приветствие пользователя "Маша"
+
+Console.Write("Введите имя пользователя: ");
+string username  = Console.ReadLine();
+
+if(username.ToLower() == "маша") //добавили оператор ".ToLower()" не учитывает регистр
+{
+     Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    Console.WriteLine("Привет," + username);
+}
