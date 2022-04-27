919d01a8e3f5dfa752be52a3d9a716212d6c4b81
Badizawr <vicodin-13@yandex.ua>
2022-04-27 15:55:45 +0500

    Exemple003

---
+++ Leson1/Exemple003_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача сумма двух чисел
+
+//Вариант 1 пример пишем в команде вывода
+//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+//Варинт2 через дополнительную переменную
+int numberA = 3;
+int numberB = 10;
+int result = numberA + numberB;
+Console.WriteLine(result);
