0237122605d9ecce3662b96a76286068b9752430
Badizawr <vicodin-13@yandex.ua>
2022-04-27 17:24:24 +0500

    add Exemple 007 Base

---
+++ Leson1/Exemple007_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿// Зада из первой лекци на нахождении тяжелой гири
+
+int a = 8;
+int b = 0;
+int c = 10;
+int d = 8;
+int e = 20;
+
+int max = a;
+
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.WriteLine("max = " + max);
