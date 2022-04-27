b5835128fb027f2a7ac05532be2607c40e40922e
Badizawr <vicodin-13@yandex.ua>
2022-04-27 16:08:29 +0500

    add Exemple 004 div

---
+++ Leson1/Exemple004_div/Program.cs
@@ -0,0 +1,11 @@
+﻿// найти частное двух чисел
+
+//Вариант 1 (не работает с дробными делит без остатка)
+//int numberA = 12;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+
+//Вариант 1 (изменили тип данных)
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
