9bd840285de4e108ab57f5ab399c23e1b70aeabc
Badizawr <vicodin-13@yandex.ua>
2022-05-09 12:55:55 +0500

    Tortle write TOPT

--- Lesson. С# fast strat/HelloWorldSolution/TurtleTraning1/Program.cs
+++ Lesson. С# fast strat/HelloWorldSolution/TurtleTraning1/Program.cs
@@ -6,11 +6,60 @@ namespace TurtleTraning1
 {
     class Program
     {
+        static void WriteT(int size) 
+        {
+            //Начало буквы Т
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.TurnLeft();
+            Turtle.Move(20);
+            Turtle.Angle = 90;
+            Turtle.Move(40);
+            //конец буквы Т
+        }
+
+        static void WriteO(int size)
+        {
+            //Буква О
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(size);
+                Turtle.TurnRight();
+            }
+        }
+
+        static void WriteP(int size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(size / 2);
+            }
+
+        }
         static void Main(string[] args)
         {
-            Turtle.Speed = 5;
-            int turtleStep = 2;
-            for (int i = 0; i < turtleStep; i++)
+            Turtle.Speed = 9;
+
+            // Торт
+            Turtle.X = 200;
+            Turtle.Y = 200;
+            WriteT(60);
+
+            Turtle.X = 220;
+            Turtle.Y = 200;
+            WriteO(30);
+
+            Turtle.X = 270;
+            Turtle.Y = 200;
+            WriteP(30);
+
+            Turtle.X = 310;
+            Turtle.Y = 200;
+            WriteT(30);
             {
                 //Рисуем забор
                 //Turtle.Move(20);
