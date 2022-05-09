ef109c63b94d0bf275c2e20b79fb115614a51f70
Badizawr <vicodin-13@yandex.ua>
2022-05-09 15:50:37 +0500

    Create Turtle game

---
+++ Lesson. С# fast strat/HelloWorldSolution/TurtleGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+          while(true)
+            {
+                Turtle.Move(10);
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if(GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if(GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if(GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+            else if(GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+
+        }
+    }
+}
