b88f2fca4cc38f8f44d17deffe37c0a2d75ea052
Badizawr <vicodin-13@yandex.ua>
2022-05-13 22:01:12 +0500

    create introArray

---
+++ LecturesIntroductionToMachineLanguage/Exemple009_IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 27;
+int c1 = 73;
+int a2 = 1757;
+int b2 = 12;
+int c2 = 32;
+int a3 = 133;
+int b3 = 22;
+int c3 = 323;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
