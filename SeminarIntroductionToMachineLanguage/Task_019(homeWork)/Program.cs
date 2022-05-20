470c1fdf266abd8946bdb7fdb4cddb67d7f6ebc7
Badizawr <vicodin-13@yandex.ua>
2022-05-20 23:42:27 +0500

    did part of the homework task 19 and 23

---
+++ SeminarIntroductionToMachineLanguage/Task_019(homeWork)/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.Write("Введите число для проверки на палиндром: ");
+string text = Console.ReadLine();
+int len = text.Length;
+bool palindrom = true;
+
+for (int i = 0; i < len / 2; i++)
+{
+    if (text[i] != text[len - (i + 1)])
+    {
+        palindrom = false;
+        break;
+    }
+}
+if (palindrom)
+{
+    Console.WriteLine("{0} -> да", text);
+}
+else
+{
+    Console.WriteLine("{0} -> нет", text);
+}
--- SeminarIntroductionToMachineLanguage/Task_021/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_021/Program.cs
@@ -4,6 +4,8 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
+static void distance()
+{
 Console.WriteLine("Введите кординаты первой точки: ");
 double x1 = double.Parse(Console.ReadLine());
 double y1 = double.Parse(Console.ReadLine());
@@ -13,3 +15,4 @@ double y2 = double.Parse(Console.ReadLine());
 double result = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 double distance = Math.Round(result, 2);
 Console.WriteLine($"Расстояние между двумя точками на плоскости равно {distance}");
+}
