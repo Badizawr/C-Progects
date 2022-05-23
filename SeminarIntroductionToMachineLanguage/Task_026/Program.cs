cf8c90c0c308fb99b2b4612836d79d7890330a55
Badizawr <vicodin-13@yandex.ua>
2022-05-24 00:06:26 +0500

    create task 26

---
+++ SeminarIntroductionToMachineLanguage/Task_026/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+System.Console.Write("Введите число:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number;
+
+int count = 0;
+for (result = number; result > 0; count++) 
+{
+    result = result /10;
+}
+System.Console.WriteLine($"В числе {number}  количество цифр = {count}");
