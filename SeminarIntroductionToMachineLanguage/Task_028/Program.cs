7fa45136f1d3728b1dfafa2fb0765b46d6001503
Badizawr <vicodin-13@yandex.ua>
2022-05-24 22:00:07 +0500

    Creat  task 28

---
+++ SeminarIntroductionToMachineLanguage/Task_028/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int SummNumber(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+int result = SummNumber(number);
+
+Console.WriteLine($"произведение чисел от 1 до {number} = {result}");
