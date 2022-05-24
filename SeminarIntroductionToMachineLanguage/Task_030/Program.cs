923409373300cdda681c580a2d8a479e4919d67f
Badizawr <vicodin-13@yandex.ua>
2022-05-24 22:21:22 +0500

    Creat  task 30

--- SeminarIntroductionToMachineLanguage/Task_030/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_030/Program.cs
@@ -3,3 +3,26 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+int N = 8;
+int min = 0;
+int max = 1;
+
+int[] CreateArray(int N, int min, int max)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(CreateArray(N, min, max));
