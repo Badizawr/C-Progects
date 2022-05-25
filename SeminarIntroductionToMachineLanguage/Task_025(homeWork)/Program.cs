e5246b03990362bc6e0479b93f6b04678b6060b6
Badizawr <vicodin-13@yandex.ua>
2022-05-25 21:40:21 +0500

    finish task 25

--- SeminarIntroductionToMachineLanguage/Task_025(homeWork)/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_025(homeWork)/Program.cs
@@ -1,3 +1,20 @@
 ﻿// Напишите цикл, который принимает на вход
 // два числа (A и B) и возводит число A в натуральную
 // степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int extent = int.Parse(Console.ReadLine());
+int count = 1;
+int result = num;
+
+while (count < extent)
+{
+    result = result * num;
+    count++;
+}
+
+System.Console.WriteLine($"{num} ^ {extent} = {result}");
