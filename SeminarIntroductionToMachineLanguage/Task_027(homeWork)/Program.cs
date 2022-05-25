2def6d6b59eac401104e65b86890e24fab200daa
Badizawr <vicodin-13@yandex.ua>
2022-05-25 21:18:32 +0500

    finish task 27

--- SeminarIntroductionToMachineLanguage/Task_027(homeWork)/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_027(homeWork)/Program.cs
@@ -1,2 +1,23 @@
 ﻿// Напишите программу, которая принимает на
 // вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumInDigit(int number)
+{
+    int sumDigit = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sumDigit = sumDigit + digit;
+        number = number / 10;
+    }
+    return sumDigit;
+}
+int sumResult = GetSumInDigit(num);
+
+Console.WriteLine($"Сумма цифр в числе {num} = {sumResult}");
