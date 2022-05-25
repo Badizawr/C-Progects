6be28d2c841656924b3dd17e117ad86826a07aaf
Badizawr <vicodin-13@yandex.ua>
2022-05-25 23:39:16 +0500

    finish task 29

--- SeminarIntroductionToMachineLanguage/Task_029(homeWork)/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_029(homeWork)/Program.cs
@@ -2,3 +2,27 @@
 // Размер массива определяется пользователем.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите длинну массива: ");
+int N = int.Parse(Console.ReadLine());
+
+int[] CreateArray(int N)
+{
+    int[] arr = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        arr[i] = new Random().Next(0,25); // установил диапазон заполнения что бы не получать большие цифры
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+Console.Write("[");
+PrintArray(CreateArray(N));
+Console.Write("]");
