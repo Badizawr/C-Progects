e1a38588a18557b4aff21a73183a594ea222cbb6
Badizawr <vicodin-13@yandex.ua>
2022-05-31 23:27:18 +0500

    Created folders with tasks 40,42

---
+++ SeminarIntroductionToMachineLanguage/Task_042/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+int result = 0;
+
+while (a > 0)
+{
+    result = a % 2;
+    a = a / 2;
+
+}
+
+Console.WriteLine(result);
---
+++ SeminarIntroductionToMachineLanguage/Task_40/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Cosole.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Cosole.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Cosole.ReadLine());
+
