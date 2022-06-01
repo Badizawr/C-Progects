0f26bf86ddc7dc35df03fc32aa1540a8e26db641
Badizawr <vicodin-13@yandex.ua>
2022-06-02 00:05:21 +0500

    finish tasks 41

--- SeminarIntroductionToMachineLanguage/Task_041(homeWork)/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_041(homeWork)/Program.cs
@@ -2,3 +2,14 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine(" Введите целые числа через пробел -> ");
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0) count += 1;
+}
+
+Console.WriteLine($"чисел больше 0 -> {count}");
