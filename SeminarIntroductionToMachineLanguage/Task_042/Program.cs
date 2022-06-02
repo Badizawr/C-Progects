21c7b9e2ee1a8dfcf97706c1b7ec448ffda4bf16
Badizawr <vicodin-13@yandex.ua>
2022-06-02 22:53:05 +0500

    Made changes to the task 42

--- SeminarIntroductionToMachineLanguage/Task_042/Program.cs
+++ SeminarIntroductionToMachineLanguage/Task_042/Program.cs
@@ -4,17 +4,26 @@
 // 3 -> 11
 // 2 -> 10
 
-Console.WriteLine("Введите первое число: ");
+Console.Write("Введите первое число: ");
 int num = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите основание системы счисления: ");
+Console.Write("Введите основание системы счисления: ");
 int baseNum = int.Parse(Console.ReadLine());
 
-int result = 0;
-int d10 = 1;
-while (num != 0)
+// int result = 0;
+// int d10 = 1;
+// while (num != 0)
+// {
+//     result = result + num % baseNum * d10;
+//     num /= baseNum;
+//     d10 *= 10;
+// }
+// Console.WriteLine(result);
+
+void DecToOther(int n, int baseN)
 {
-    result = result + num % baseNum * d10;
-    num /= baseNum;
-    d10 *= 10;
+    if (n == 0) return;
+    DecToOther(n / baseN, baseN);
+    Console.Write(n % baseN);
 }
-Console.WriteLine(result);
+
+DecToOther(num, baseNum);
