ed93ce1495515acd61c6829e94f7a57eeb56b093
Badizawr <vicodin-13@yandex.ua>
2022-06-02 23:47:56 +0500

    created folders for tasks 44 and 45

---
+++ SeminarIntroductionToMachineLanguage/Task_044/Program.cs
@@ -0,0 +1,18 @@
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("До какого числа делаем вывод: ");
+int size = int.Parse(Console.ReadLine());
+int nums = 0;
+int nums2 = 1;
+int[] numbers = new int[size];
+//int count = 0;
+
+for (int i = 0; i <= size; i++)
+{
+    nums2 = nums2 + nums;
+    
+}
