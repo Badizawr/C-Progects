72d7f4fd7445ceb700daaae0b25a826d5a746ee4
Badizawr <vicodin-13@yandex.ua>
2022-06-06 23:11:27 +0500

    finish task 46 and 48

---
+++ SeminarIntroductionToMachineLanguage/Task_048/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 5;
+int n = 5;
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
