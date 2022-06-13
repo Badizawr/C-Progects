4b986306ceb20cbed73397e5ef05f6ef85ca7ffc
Badizawr <vicodin-13@yandex.ua>
2022-06-13 23:41:46 +0500

    finish task 67

---
+++ SeminarIntroductionToMachineLanguage/Task_067/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+SumNumber(n, 0);
+
+void SumNumber(int num, int sum)
+{
+    if (num == 0)
+    {
+        Console.WriteLine($"сумма числа равна -> {sum}");
+        return;
+    }
+
+    sum = sum + num % 10;
+    num = num / 10;
+    SumNumber(num, sum);
+}
